97168aa05d40da6b05be4fa062a0d2e0d8f0b365
hoa.dinh_dev <xuanhoa1697@gmail.com>
2024-02-20 22:37:00 +0700

    Up

--- apk_login_controller/__manifest__.py
+++ apk_login_controller/__manifest__.py
@@ -8,6 +8,7 @@
     "depends": ["base", "web"],
     "installable": True,
     'license': 'LGPL-3',
+    'price': 750,
     'currency': 'EUR',
     'apkLink': 'https://1drv.ms/u/s!AvqczWuUKhgmgiZp6r5Otu9SYziH?e=kV9xcd',
     'images': [
